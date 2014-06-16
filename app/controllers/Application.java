0f133654043e991a64e512b54b34ee1b36c7968d
Yauri Attamimi <yauritux@gmail.com>
2014-06-17 01:38:46 +0700

    Dashboard

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import models.*;
+
 import play.*;
 import play.mvc.*;
 
@@ -8,7 +10,10 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("ZenTasks will be here soon"));
+       return ok(index.render(
+          Project.find.all(), 
+          Task.find.all()
+       ));
     }
 
 }
